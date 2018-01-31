1c9edd151f11a9b8ff64c32af7bb00f78694bdb1
Chris <chris.tsimogiannis@rmb.co.za>
2018-01-31 16:30:12 +0200

    added participant store interface

---
+++ contracts/ParticipantStoreInterface.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.17;
+
+// ParticipantStore
+//  - Stores a list of participants
+//  - only owner can add / remove participants
+//  - anybody can get accounts and details of participants
+
+interface ParticipantStoreInterface {
+
+    function addParticipant(address account, bytes32 name) public;
+    function removeParticipant(address account) public;
+
+    function getParticipant(address account) public view returns (address, bytes32);
+    function getParticipantAccounts() public view returns (address[]);
+    function getParticipantCount() public view returns (uint);
+
+}
